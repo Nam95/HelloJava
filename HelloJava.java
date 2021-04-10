f4d48699b90044a31c45d381e03e84428ea68116
Nam95 <33070228+Nam95@users.noreply.github.com>
2021-04-10 16:00:45 +0530

    Update HelloJava.java

--- HelloJava.java
+++ HelloJava.java
@@ -3,6 +3,6 @@ public class HelloJava{
      System.out.println("Hello Java");
      System.out.println("I am learning how to use Build triggers");
      System.out.println("Third statement");
-     System.out.println("Adding one statement to check if build triggers on a commit");
+     System.out.println("Adding one statement11 to check if build triggers on a commit");
     }  
 }  
