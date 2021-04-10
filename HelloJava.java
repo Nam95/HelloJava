939f1118cccd012afd259f15c6815638fb15a380
Nam95 <33070228+Nam95@users.noreply.github.com>
2021-04-10 15:54:01 +0530

    Update HelloJava.java

--- HelloJava.java
+++ HelloJava.java
@@ -1,4 +1,4 @@
-class Simple{  
+public class HelloJava{  
     public static void main(String args[]){  
      System.out.println("Hello Java");
      System.out.println("I am learning how to use Build triggers");
